b15801df0411a2009e8bad4787128e8842b76a20
Ryan Breaker <ryan@breaker.rocks>
2015-11-26 23:06:37 -0600

    Cat

--- cat/cat.go
+++ cat/cat.go
@@ -2,18 +2,14 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
+	"strings"
 )
 
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
-
 func main() {
-
 	flags := make(map[string]*bool)
 
 	flags["b"] = flag.Bool("b", false, "Number the non-blank output lines, starting at 1.")
@@ -26,8 +22,53 @@ func main() {
 	// Parse 'em
 	flag.Parse()
 
-	// Real program stuff
+	// Read from Stdin, spit back out to Stdout
+	if flag.NArg() == 0 || (flag.NArg() == 1 && flag.Arg(0) == "-") {
+		io.Copy(os.Stdout, os.Stdin)
+		os.Exit(0)
+	}
 
-	io.Copy(os.Stdout, os.Stdin)
+	// Cat files
+	outputString := ""
+	lineNum := 1
+	for _, arg := range flag.Args() {
+		argBytes, err := ioutil.ReadFile(arg)
+		if err != nil {
+			os.Stderr.WriteString("File '" + arg + "' does not exist!\n")
+			continue
+		}
 
+		argString := string(argBytes)
+
+		// Print \t as ^I
+		if *flags["t"] {
+			argString = strings.Replace(argString, "\t", "^I", -1)
+		}
+
+		// Number each line
+		if *flags["n"] {
+			// Parse argString
+			for _, line := range strings.Split(argString, "\n") {
+				// Skip blank lines
+				if *flags["b"] && line == "" {
+					continue
+				}
+				// Add output
+				outputString += fmt.Sprintf("\t%d  %s\n", lineNum, line)
+
+				lineNum++
+			}
+
+		} else {
+			// Just add to output
+			outputString += argString
+		}
+	}
+
+	// Print \n as $
+	if *flags["e"] {
+		outputString = strings.Replace(outputString, "\n", "$\n", -1)
+	}
+
+	fmt.Print(outputString)
 }
